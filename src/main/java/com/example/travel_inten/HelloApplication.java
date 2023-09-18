09b0d722d2e1e1d61cba863548ed0fea63984bfd
waelgithub123 <waeldkhili0200@gmail.com>
2023-09-18 01:11:53 -0400

    added new window size and added panels based on figma

--- src/main/java/com/example/travel_inten/HelloApplication.java
+++ src/main/java/com/example/travel_inten/HelloApplication.java
@@ -9,13 +9,24 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
+//    public void start(Stage stage) throws IOException {
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        stage.setTitle("Hello!");
+//        stage.setScene(scene);
+//        stage.show();
+//    }
+
+    //function that creates a window that is 1920 x 1080
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 700);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
+
+
 
     public static void main(String[] args) {
         launch();
