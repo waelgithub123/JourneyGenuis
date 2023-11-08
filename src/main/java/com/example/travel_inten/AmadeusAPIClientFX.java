b4e45c3988a5a05cee32c318de40b87bc88e138e
waelgithub123 <waeldkhili0200@gmail.com>
2023-11-08 01:10:37 -0500

    added a new APi for the flight data, a bunch of new errors have spawned and I have no idea what to do, hopefully I can reconsile this

--- src/main/java/com/example/travel_inten/AmadeusAPIClientFX.java
+++ src/main/java/com/example/travel_inten/AmadeusAPIClientFX.java
@@ -107,102 +107,213 @@ package com.example.travel_inten;
 //    }
 //}
 
+//----------------------------------------------------------------------
+
+//import javafx.application.Application;
+//import javafx.stage.Stage;
+//import javafx.scene.Scene;
+//import javafx.scene.control.*;
+//import javafx.scene.layout.*;
+//import javafx.geometry.Insets;
+//import java.io.*;
+//import java.net.HttpURLConnection;
+//import java.net.URL;
+//import java.util.stream.Collectors;
+//
+//public class AmadeusAPIClientFX extends Application {
+//
+//    private static final String API_BASE_URL = "https://api.aviationstack.com/v1/flights";
+//    private static final String API_KEY = "bf9cbf866a16e7ca7b6ad3166660a3f9";
+//
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+//
+//    @Override
+//    public void start(Stage primaryStage) {
+//        primaryStage.setTitle("Flight Information App");
+//
+//        Label departureLabel = new Label("Departure Location:");
+//        Label returnLabel = new Label("Return Location:");
+//        Label departureTimeLabel = new Label("Departure Time (YYYY-MM-DD):");
+//        Label returnTimeLabel = new Label("Return Time (YYYY-MM-DD):");
+//
+//        TextField departureLocationField = new TextField();
+//        TextField returnLocationField = new TextField();
+//        TextField departureTimeField = new TextField();
+//        TextField returnTimeField = new TextField();
+//
+//        Button searchButton = new Button("Search");
+//        TextArea resultArea = new TextArea();
+//
+//        searchButton.setOnAction(e -> {
+//            String departureLocation = departureLocationField.getText();
+//            String returnLocation = returnLocationField.getText();
+//            String departureTime = departureTimeField.getText();
+//            String returnTime = returnTimeField.getText();
+//
+//            if (!departureLocation.isEmpty() && !returnLocation.isEmpty() && !departureTime.isEmpty() && !returnTime.isEmpty()) {
+//                String apiUrl = String.format(
+//                        "%s?access_key=%s&dep_iata=%s&arr_iata=%s&flight_date=%s&arr_scheduled_time_arr=%s",
+//                        API_BASE_URL, API_KEY, departureLocation, returnLocation, departureTime, returnTime);
+//
+//                try {
+//                    String response = sendGetRequest(apiUrl);
+//                    resultArea.setText(response);
+//                } catch (IOException ex) {
+//                    resultArea.setText("Error: " + ex.getMessage());
+//                }
+//            } else {
+//                resultArea.setText("Please fill in all fields.");
+//            }
+//        });
+//
+//        GridPane grid = new GridPane();
+//        grid.setHgap(10);
+//        grid.setVgap(10);
+//        grid.setPadding(new Insets(10, 10, 10, 10));
+//        grid.add(departureLabel, 0, 0);
+//        grid.add(returnLabel, 0, 1);
+//        grid.add(departureTimeLabel, 0, 2);
+//        grid.add(returnTimeLabel, 0, 3);
+//        grid.add(departureLocationField, 1, 0);
+//        grid.add(returnLocationField, 1, 1);
+//        grid.add(departureTimeField, 1, 2);
+//        grid.add(returnTimeField, 1, 3);
+//        grid.add(searchButton, 0, 4, 2, 1);
+//
+//        VBox vbox = new VBox(10);
+//        vbox.getChildren().addAll(grid, resultArea);
+//
+//        Scene scene = new Scene(vbox, 500, 400);
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
+//    }
+//
+//    private String sendGetRequest(String apiUrl) throws IOException {
+//        URL url = new URL(apiUrl);
+//        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//        connection.setRequestMethod("GET");
+//
+//        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+//            return reader.lines().collect(Collectors.joining("\n"));
+//        } finally {
+//            connection.disconnect();
+//        }
+//    }
+//}
+
+
 import javafx.application.Application;
-import javafx.geometry.Insets;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.opensky.api.OpenSkyApi;
+import org.opensky.api.OpenSkyStates;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.stream.Collectors;
+public class FlightDataViewer extends Application {
 
-public class AmadeusAPIClientFX extends Application {
-    private final String API_KEY = "bf9cbf866a16e7ca7b6ad3166660a3f9";
-    private final String API_BASE_URL = "https://api.aviationstack.com/v1/flights";
+    private final String username = "your_username";
+    private final String password = "your_password";
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Flight Information App");
+    public void start(Stage stage) {
+        stage.setTitle("Flight Data Viewer");
 
-        Label departureLabel = new Label("Departure Location:");
-        Label returnLabel = new Label("Return Location:");
-        Label departureTimeLabel = new Label("Departure Time (YYYY-MM-DD):");
-        Label returnTimeLabel = new Label("Return Time (YYYY-MM-DD):");
+        // Create a TableView to display flight data
+        TableView<FlightData> tableView = new TableView<>();
+        ObservableList<FlightData> flightDataList = FXCollections.observableArrayList();
 
-        TextField departureLocationField = new TextField();
-        TextField returnLocationField = new TextField();
-        TextField departureTimeField = new TextField();
-        TextField returnTimeField = new TextField();
+        TableColumn<FlightData, String> icao24Col = new TableColumn<>("ICAO24");
+        icao24Col.setCellValueFactory(cellData -> cellData.getValue().icao24Property());
 
-        Button searchButton = new Button("Search");
-        TextArea resultArea = new TextArea();
+        TableColumn<FlightData, String> callsignCol = new TableColumn<>("Callsign");
+        callsignCol.setCellValueFactory(cellData -> cellData.getValue().callsignProperty());
 
-        searchButton.setOnAction(e -> {
-            String departureLocation = departureLocationField.getText();
-            String returnLocation = returnLocationField.getText();
-            String departureTime = departureTimeField.getText();
-            String returnTime = returnTimeField.getText();
+        TableColumn<FlightData, Double> latitudeCol = new TableColumn<>("Latitude");
+        latitudeCol.setCellValueFactory(cellData -> cellData.getValue().latitudeProperty().asObject());
 
-            if (!departureLocation.isEmpty() && !returnLocation.isEmpty() && !departureTime.isEmpty() && !returnTime.isEmpty()) {
-                String apiUrl = String.format(
-                        "%s?access_key=%s&dep_iata=%s&arr_iata=%s&flight_date=%s&arr_scheduled_time_arr=%s",
-                        API_BASE_URL, API_KEY, departureLocation, returnLocation, departureTime, returnTime);
+        TableColumn<FlightData, Double> longitudeCol = new TableColumn<>("Longitude");
+        longitudeCol.setCellValueFactory(cellData -> cellData.getValue().longitudeProperty().asObject());
 
-                try {
-                    String response = sendGetRequest(apiUrl);
-                    resultArea.setText(response);
-                } catch (IOException ex) {
-                    resultArea.setText("Error: " + ex.getMessage());
-                }
-            } else {
-                resultArea.setText("Please fill in all fields.");
-            }
+        tableView.getColumns().addAll(icao24Col, callsignCol, latitudeCol, longitudeCol);
+        tableView.setItems(flightDataList);
+
+        // Fetch and display flight data
+        OpenSkyApi api = new OpenSkyApi(username, password);
+        OpenSkyStates states = api.getStates(0, null);
+
+        states.getStates().forEach(state -> {
+            flightDataList.add(new FlightData(
+                    state.getIcao24(),
+                    state.getCallsign(),
+                    state.getLatitude(),
+                    state.getLongitude()
+            ));
         });
 
-        GridPane grid = new GridPane();
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(10, 10, 10, 10));
-        grid.add(departureLabel, 0, 0);
-        grid.add(returnLabel, 0, 1);
-        grid.add(departureTimeLabel, 0, 2);
-        grid.add(returnTimeLabel, 0, 3);
-        grid.add(departureLocationField, 1, 0);
-        grid.add(returnLocationField, 1, 1);
-        grid.add(departureTimeField, 1, 2);
-        grid.add(returnTimeField, 1, 3);
-        grid.add(searchButton, 0, 4, 2, 1);
-
-        VBox vbox = new VBox(10);
-        vbox.getChildren().addAll(grid, resultArea);
-
-        Scene scene = new Scene(vbox, 500, 400);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        // Create a layout and set it as the scene
+        VBox vbox = new VBox(tableView);
+        Scene scene = new Scene(vbox, 800, 600);
+        stage.setScene(scene);
+        stage.show();
     }
 
-    private String sendGetRequest(String apiUrl) throws IOException {
-        URL url = new URL(apiUrl);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
+    public static class FlightData {
+        private final String icao24;
+        private final String callsign;
+        private final double latitude;
+        private final double longitude;
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-            return reader.lines().collect(Collectors.joining("\n"));
-        } finally {
-            connection.disconnect();
+        public FlightData(String icao24, String callsign, double latitude, double longitude) {
+            this.icao24 = icao24;
+            this.callsign = callsign;
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+
+        public String getIcao24() {
+            return icao24;
+        }
+
+        public String getCallsign() {
+            return callsign;
+        }
+
+        public double getLatitude() {
+            return latitude;
+        }
+
+        public double getLongitude() {
+            return longitude;
+        }
+
+        public StringProperty icao24Property() {
+            return new SimpleStringProperty(icao24);
+        }
+
+        public StringProperty callsignProperty() {
+            return new SimpleStringProperty(callsign);
+        }
+
+        public DoubleProperty latitudeProperty() {
+            return new SimpleDoubleProperty(latitude);
+        }
+
+        public DoubleProperty longitudeProperty() {
+            return new SimpleDoubleProperty(longitude);
         }
     }
 }
